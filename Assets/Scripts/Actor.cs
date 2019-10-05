ea606bce1025f6713f2750d1b3a8997a1e789ccf
Jigurd <sigurd.stapnes@gmail.dom>
2019-10-05 12:24:13 +0200

    Add gravity

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    //controls how fast actor falls
+    [Range(0.05f, 0.5f)]
+    public float gravity;
+    Vector3 velocity;
+
+    Controller controller;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //apply gravity to velocity
+        velocity.y += gravity*Time.deltaTime;
+        //move player
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector3 velocity)
+    {
+
+        transform.Translate(velocity);
+    }
+}
