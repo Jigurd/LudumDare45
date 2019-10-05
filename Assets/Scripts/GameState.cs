7773d60587c26606c1d0ed7bc843ebc9967f8fa9
Mona Lisa <benjaminbergzh@gmail.com>
2019-10-05 15:36:40 +0200

    Add pause functionality

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// Represents the current state of the game.
+/// </summary>
+public static class GameState
+{
+    // Whether or not the game is paused
+    public static bool Paused = false;
+}
---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Whether the pause menu is enabled or not
+    private bool _enabled = false;
+
+    private void Awake()
+    {
+        // Set the pause menu to its desired inital state.
+        // Might as well hardcode this to false really.
+        SetPauseMenuEnabled(_enabled);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            // Toggle the pause menu
+            SetPauseMenuEnabled(!_enabled);
+        }
+    }
+
+    /// <summary>
+    /// Toggles the pause menu.
+    /// </summary>
+    /// <param name="enabled">The new state of the pause menu.</param>
+    private void SetPauseMenuEnabled(bool enabled)
+    {
+        _enabled = enabled;
+        // Set each child to the desired state
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(enabled);
+        }
+
+        if (_enabled)
+        {
+            // Pause the game
+            GameState.Paused = true;
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            // Resume the game
+            GameState.Paused = false;
+            Time.timeScale = 1.0f;
+        }
+    }
+}
