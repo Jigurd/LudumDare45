72e2d5edc91a0444ec6651590bd21a8c10c5cd9c
Mona Lisa <benjaminbergzh@gmail.com>
2019-10-05 11:01:09 +0200

    Add main menu

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Handles behaviour for the main menu play button.
+/// </summary>
+public class PlayButton : MonoBehaviour
+{
+    // The scene to load when the play button is pressed.
+    [SerializeField] private int _indexOfSceneToLoad = 1;
+
+
+    private void Awake()
+    {
+        // Load scene when button is pressed.
+        GetComponent<Button>().onClick.AddListener(
+            delegate { SceneManager.LoadScene(_indexOfSceneToLoad); }
+        );
+    }
+}
---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Handles behaviour for the quit button.
+/// </summary>
+public class QuitButton : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Quit the game when button pressed.
+        GetComponent<Button>().onClick.AddListener(
+            delegate
+            {
+                Debug.Log("Quit the application");
+                Application.Quit();
+            }
+        );
+    }
+}
