24c1507de3ab0feb62e119d239a4fad80c018d1b
Mona Lisa <benjaminbergzh@gmail.com>
2019-10-05 12:54:05 +0200

    Update line renderer positions when drawing moves

--- Assets/Scripts/Drawing/Drawing.cs
+++ Assets/Scripts/Drawing/Drawing.cs
@@ -8,14 +8,42 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class Drawing : MonoBehaviour
 {
+    // The previous position of the drawing
+    private Vector3 _previousPosition = Vector3.zero;
+
     // The line renderer representing the drawing
-    LineRenderer _lineRenderer = null;
+    private LineRenderer _lineRenderer = null;
 
     private void Awake()
     {
+        // Set up
         _lineRenderer = GetComponent<LineRenderer>();
+        _previousPosition = transform.position;
     }
 
+    // Runs every frame
+    void Update()
+    {
+        // Get amount the transform has moved this frame
+        Vector3[] points = new Vector3[_lineRenderer.positionCount];
+        _lineRenderer.GetPositions(points);
+        Vector3 movementSinceLastFrame = transform.position - _previousPosition;
+
+        // If we have moved
+        if (movementSinceLastFrame != Vector3.zero)
+        {
+            // Set line renderer positions to its original positions
+            // plus the amount we have moved
+            for (int i = 0; i < _lineRenderer.positionCount; i++)
+            {
+                points[i] += movementSinceLastFrame;
+            }
+            _lineRenderer.SetPositions(points);
+
+            // Update our previous position
+            _previousPosition = transform.position;
+        }
+    }
 
     /// <summary>
     /// Adds a point to the drawing.
