b7550718e22f8c952e899a0535d2bff4fb611cd9
Jigurd <sigurd.stapnes@gmail.dom>
2019-10-05 17:27:59 +0200

    Added door functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     public GameObject SpawnPoint;
 
-    
+    //door stuff
+    [SerializeField]
+    public GameObject Door; //the level exit
+    public LayerMask DoorMask;  //the layermask that looks for it
+
+
 
     void Start()
     {
@@ -49,16 +54,42 @@ public class PlayerController : MonoBehaviour
         {
             actor.velocity.x += actor.moveSpeed;
         }
+
+        //
+        if (Input.GetButtonDown("Interact"))
+        {
+            CheckIfWon();
+        }
+    }
+
+    //checks if the player is standing by the exit
+    void CheckIfWon()
+    {
+        //cast a ray from player's position toward door's position, with a length of half the player's size
+        Vector3 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(pos, (Door.transform.position - pos).normalized, 0.5f, DoorMask);
+        Debug.DrawRay(pos, (Door.transform.position - pos).normalized* 0.5f, Color.red);
+
+        if(hit)
+        {
+            WinLevel();
+        }
+        //if it hits, win
     }
 
     //checks if the player is still in the level
     void CheckInBounds()
     {
         //if character is far out of bounds
-        if (Mathf.Abs(transform.position.x) > 40 || Mathf.Abs(transform.position.y)>40)
+        if (Mathf.Abs(transform.position.x) > 40 || Mathf.Abs(transform.position.y) > 40)
         {
             actor.velocity = new Vector3(0, 0); //reset speed
             transform.position = SpawnPoint.transform.position;
         }
+    }
+
+    void WinLevel()
+    {
+        Debug.Log("Yay, you win!");
     }
 }
