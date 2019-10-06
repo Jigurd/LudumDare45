38a57936d1a427b981227cd7e4acf9ffc8239b35
Jigurd <sigurd.stapnes@gmail.dom>
2019-10-06 10:44:58 +0200

    Progress on floater

--- Assets/Scripts/Enemies/Floater.cs
+++ Assets/Scripts/Enemies/Floater.cs
@@ -4,23 +4,51 @@ using UnityEngine;
 
 public class Floater : MonoBehaviour
 {
-    Controller controller; 
+    private Controller _controller;
+    private GameObject _player;
+    private SpriteRenderer _spriteRenderer;
 
-    public Vector3 velocity;
     [SerializeField]
     public float moveSpeed;
 
-    public LayerMask layermask;
-    // Start is called before the first frame update
+    private LayerMask _layerMask;
+
     void Start()
     {
-        controller = GetComponent<Controller>();
+        _controller = GetComponent<Controller>();
+        _player = GameObject.Find("Player");
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        //get our position
+        Vector3 pos = transform.position;
+        Vector3 dir = (_player.transform.position - pos).normalized;
+        //raycast to player
+        RaycastHit2D hit = Physics2D.Raycast(pos, dir , Mathf.Infinity, _layerMask);
+        Debug.DrawRay(pos, dir * 10, Color.red);
 
-        controller.Move(velocity * Time.deltaTime);
+        //if player isn't looking at us, become green and move towards him
+        if (hit && !hit.collider.CompareTag("Player"))
+        { 
+            _spriteRenderer.color = Color.green;
+            _controller.Move(dir * moveSpeed * Time.deltaTime);
+        } else if (hit && hit.collider.CompareTag("Player")) //if he is looking at us
+        {
+            //turn red and don't move
+            _spriteRenderer.color = Color.red;
+        }
+        else
+        {
+            Debug.Log("Wtf no hit");
+        }
+        //If we hit anything in our LayerMask other than Player, we can't see him
+        
+        //move speed towards him
+
+
+        
     }
 }
