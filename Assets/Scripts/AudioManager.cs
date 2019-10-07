f3afb220f2a6a284a2adb515a65cb1d9a0b3fff6
Mona Lisa <benjaminbergzh@gmail.com>
2019-10-07 11:58:14 +0200

    Add music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Our main theme
+    [SerializeField] AudioClip _themeIntro = null;
+    [SerializeField] AudioClip _themeEnding = null;
+    [SerializeField] AudioClip _themeLoop = null;
+
+    // Currently playing AudioSources and when to kill them
+    private Dictionary<AudioSource, float> _audioSources = null;
+
+    // Delayes audio sources
+    private Dictionary<AudioSource, float> _delayedAudioSources = null;
+
+    private void Awake()
+    {
+        _audioSources = new Dictionary<AudioSource, float>();
+        _delayedAudioSources = new Dictionary<AudioSource, float>();
+        Play(_themeIntro);
+        Play(_themeLoop, true, _themeIntro.length);
+    }
+
+    private void Update()
+    {
+        AudioSource[] sources = _audioSources.Keys.ToArray();
+        foreach (AudioSource source in sources)
+        {
+            if (Time.time >= _audioSources[source])
+            {
+                Stop(source);
+            }
+        }
+    }
+
+    private void Play(AudioClip clip, bool loop = false, float delay = 0.0f)
+    {
+        AudioSource source = gameObject.AddComponent<AudioSource>();
+        source.loop = loop;
+        source.clip = clip;
+        source.PlayDelayed(delay);
+        if (!loop)
+        {
+            _audioSources.Add(source, Time.time + clip.length + delay);
+        }
+    }
+
+    private void Stop(AudioSource audioSource)
+    {
+        _audioSources.Remove(audioSource);
+        Destroy(audioSource);
+    }
+}
